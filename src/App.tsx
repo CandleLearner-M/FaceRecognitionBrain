315881b0d4ff3a5e27d9a8846ee1f94191dccced
Mostafa <candlelearner.m.dev@gmail.com>
2025-07-04 13:07:46 +0100

    working on imagelinkform

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
 
 
 function App() {
   
 
   return (
-    <>
-    
-    </>
+    <main>
+      <div className='pattern-particle-wave'></div>
+      <div className='content-wrapper'>
+        <Navigation />
+        <ImageLinkForm />
+        {/* <Logo />
+        <FaceRecognition /> */}
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,10 @@
+import styles from './Navigation.module.scss';
+
+function Navigation() {
+  return (
+    <nav className={styles.nav}>
+      <p>Sign Out</p>
+    </nav>
+  )
+}
+export default Navigation;
